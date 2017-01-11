ba1f7dde941c62495dcbf6b8e1091183c805030f
Wang Tengyu <tengyunb@gmail.com>
2017-01-11 14:22:14 +0800

    2.1

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #chmod u+x trigger.sh
 #nohup /weitac_gateway/trigger.sh > /weitac_gateway/trigger.date &
-if "$MIGRAGEDB" in "true"; then
-    /usr/bin/python /weitac_gateway/manage.py migrate --settings=weitac_gateway.settings
-fi
+#if "$MIGRAGEDB" in "true"; then
+#    /usr/bin/python /weitac_gateway/manage.py migrate --settings=weitac_gateway.settings
+#fi
 
 /usr/local/bin/supervisord --nodaemon # Run supervisord in the foreground
 
